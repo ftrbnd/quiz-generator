03bcc0f23e45a6e0aa8dded975b1e05152e3f2be
ftrbnd <giosalas25@gmail.com>
2025-12-03 13:42:28 -0800

    Create test files for 3 phases

---
+++ tests/preprocessing_test.py
@@ -0,0 +1,20 @@
+class TestPreprocessingPhase:
+    def test_story_2(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_12(self):
+        x = "this"
+        assert "a" in x
+    
+    def test_story_13(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_16(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_65(self):
+        x = "this"
+        assert "a" in x
---
+++ tests/question_creation_test.py
@@ -0,0 +1,20 @@
+class TestQuestionCreationPhase:
+    def test_story_9(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_10(self):
+        x = "this"
+        assert "a" in x
+    
+    def test_story_11(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_22(self):
+        x = "this"
+        assert "a" in x
+
+    def test_story_43(self):
+        x = "this"
+        assert "a" in x
