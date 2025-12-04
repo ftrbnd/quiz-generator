e334ae6545ab1a585d79769206467a9cd265ee00
Rey Sayed <105511761+streethonda@users.noreply.github.com>
2025-12-04 12:50:04 -0800

    Add files via upload
    Signed-off-by: Rey Sayed <105511761+streethonda@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -1,16 +1,15 @@
 import gradio as gr
-
-import inputs.text_tab as text_tab, inputs.file_tab as file_tab
+import inputs.text_tab as text_tab
+import inputs.file_tab as file_tab
 
 with gr.Blocks(title="Automatic Quiz Generator") as demo:
     gr.Markdown("""
-        # COMP 582 - Automatic Quiz Generator
-        ### Select your input method by selecting a tab
+        # Automatic Quiz Generator  
+        Use the **Text** or **File** tab below to enter content, then generate a quiz automatically.
     """)
-    
     with gr.Tabs():
         text_tab.render()
         file_tab.render()
 
 if __name__ == "__main__":
-    demo.launch(theme=gr.themes.Soft())
+    demo.launch(theme=gr.themes.Soft(), share=True)
